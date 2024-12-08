8aa7bcc1f0b1899bb40402c5f257954ef567dd08
shiponirfan <shiponirfan.dev@gmail.com>
2024-12-08 21:16:16 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/shiponirfan/google_map_and_geolocator/google_map_and_geolocator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.shiponirfan.google_map_and_geolocator.google_map_and_geolocator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_map_and_geolocator/views/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Real-Time Location Tracker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
